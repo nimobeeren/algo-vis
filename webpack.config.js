bdab16ccebced8175758856c467e2b1f3ccc24a4
Nimo Beeren <nimo@deity.io>
2020-10-03 18:30:43 +0200

    use webpack for production build

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
   // you didn't know this
   mode: 'production',
   output: {
-    path: path.join(__dirname, "bundleOutput"),
-    filename: 'index.js',
+    path: path.resolve(__dirname, "dist"),
+    filename: 'main.js',
   },
 };
